d2e037e0973d51a85fd985e8902ed24c07d22ea8
Lev Stolpner <l.a.stolpner@gmail.com>
2018-04-30 02:23:26 +0300

    Simple test cases from one thread

--- src/ru/stolpner/task/yandex/EventRecorderTest.java
+++ src/ru/stolpner/task/yandex/EventRecorderTest.java
@@ -6,5 +6,21 @@ public class EventRecorderTest {
         System.out.println("Empty recorder get last minute stats: " + recorder.getNumberOfLastMinuteEvents());
         System.out.println("Empty recorder get last hour stats: " + recorder.getNumberOfLastHourEvents());
         System.out.println("Empty recorder get last day stats: " + recorder.getNumberOfLastDayEvents());
+
+        recorder.recordEvent(System.currentTimeMillis());
+        recorder.recordEvent(System.currentTimeMillis());
+        recorder.recordEvent(System.currentTimeMillis());
+
+        System.out.println("3 records last minute stats: " + recorder.getNumberOfLastMinuteEvents());
+        System.out.println("3 records last hour stats: " + recorder.getNumberOfLastHourEvents());
+        System.out.println("3 records last day stats: " + recorder.getNumberOfLastDayEvents());
+
+        recorder.recordEvent(System.currentTimeMillis() - 50 * 1000);
+        recorder.recordEvent(System.currentTimeMillis() - 50 * 60 * 1000);
+        recorder.recordEvent(System.currentTimeMillis() - 20 * 60 * 60 * 1000);
+
+        System.out.println("4 records last minute stats: " + recorder.getNumberOfLastMinuteEvents());
+        System.out.println("5 records last hour stats: " + recorder.getNumberOfLastHourEvents());
+        System.out.println("6 records last day stats: " + recorder.getNumberOfLastDayEvents());
     }
 }
